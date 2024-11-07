05f50ceb04aaf36251edf6586432560018cccc3f
Bruno Duarte <bruno.duarte@bis2bis.com.br>
2024-11-07 15:32:45 -0300

    ➕ Adding constant icons

---
+++ src/constants/icons.js
@@ -0,0 +1,54 @@
+import arrowBack     from "../assets/images/icon-arrow-back.png";
+import arrowBack2    from "../assets/images/icon-arrow-back2.png";
+import arrowForward  from "../assets/images/icon-arrow-forward.png";
+import arrowForward2 from "../assets/images/icon-arrow-forward2.png";
+import close         from "../assets/images/icon-close.png";
+import creditCard    from "../assets/images/icon-credit-card.png";
+import remove        from "../assets/images/icon-delete.png";
+import favorite      from "../assets/images/icon-favorite.png";
+import home          from "../assets/images/icon-home.png";
+import listAlt       from "../assets/images/icon-list-alt.png";
+import locationOn    from "../assets/images/icon-location-on.png";
+import menu          from "../assets/images/icon-menu.png";
+import minus         from "../assets/images/icon-minus.png";
+import moreVert      from "../assets/images/icon-more-vert.png";
+import notifications from "../assets/images/icon-notifications.png";
+import payments      from "../assets/images/icon-payments.png";
+import plus          from "../assets/images/icon-plus.png";
+import qrCode        from "../assets/images/icon-qrcode-scanner.png";
+import search        from "../assets/images/icon-search.png";
+import settings      from "../assets/images/icon-settings.png";
+import shoppingBag   from "../assets/images/icon-shopping-bag.png";
+import shoppingCart  from "../assets/images/icon-shopping-cart.png";
+import shoppingMode  from "../assets/images/icon-shoppingmode.png";
+import moneyCurrency from "../assets/images/icon-universal-currency-alt.png";
+import user          from "../assets/images/icon-user.png";
+
+
+export default {
+  arrowBack,
+  arrowBack2,
+  arrowForward,
+  arrowForward2,
+  close,
+  creditCard,
+  remove,
+  favorite,
+  home,
+  listAlt,
+  locationOn,
+  menu,
+  minus,
+  moreVert,
+  notifications,
+  payments,
+  plus,
+  qrCode,
+  search,
+  settings,
+  shoppingBag,
+  shoppingCart,
+  shoppingMode,
+  moneyCurrency,
+  user
+}
